3ae2df5155bfd5fc4db19e5312f82b1e71ae462a
Gabriel Delgado <gabriel.delgado@jalasoft.com>
2014-02-19 20:16:59 -0400

    The tool to comunicate monit with websockets is being tested

--- test3/index.js
+++ test3/index.js
@@ -20,6 +20,7 @@ var app = express.createServer();
 var socket = require('socket.io');
 app.configure(function(){
   app.use(express.static(__dirname + '/'));
+  app.use(express.bodyParser());
 });
 var server = app.listen(8081);
 var io = socket.listen(server);
@@ -29,4 +30,19 @@ io.sockets.on('connection', function (socket) {
   socket.on('disconnect', function (socket) {
     console.log("disconnect");
   });
+});
+
+// POST
+app.post('/api/alert', function(req, res) {
+
+//curl -X POST "http://localhost:8081/api/alert?name=gabriel&lastname=delgado"
+//  console.log('name', req.param('name'));
+//  console.log('lastname', req.param('lastname'));
+
+    //curl -d '{"MyKey":"My Value"}' -H "Content-Type: application/json" http://localhost:8081/api/alert
+    console.log(req.body);
+    console.log('request =' + JSON.stringify(req.body));
+
+    io.sockets.emit('message', { message: req.body });
+    res.send(200);
 });
